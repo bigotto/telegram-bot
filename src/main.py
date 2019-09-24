27349310f5818049c35aed635324378207fac58c
Bruno Bigotto <bbigotto@gmail.com>
2019-09-24 15:03:07 -0400

    Escrevendo... durante a execução do comando

--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
+#coding: utf-8
 import telegram
 from telegram.ext import Updater, CommandHandler
 import speedtest
-#import os
 
-# from functools import wraps
-# def send_typing_action(func):
-#     @wraps(func)
-#     def command_func(update, context, *args, **kwargs):
-#         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=telegram.ChatAction.TYPING)
-#         return func(update, context,  *args, **kwargs)
-    
-#     return command_func
+def typing(bot, update):
+    # Send typing action aprox for 6s
+    bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+
 def start(bot, update):
     me = bot.get_me()
     # Message
     msg = ("Olá!\n")
-    msg += ('Eu sou %s e estou aqui para ajudá-lo\n' % me.first_name)
+    #msg += ('Eu sou %s e estou aqui para ajudá-lo\n' % me.first_name)
     msg += ('Comandos disponíveis:\n')
     msg += ('/speedtest - Velocidade da internet')
+    
     # Commands menu
     main_menu_keyborad = [[telegram.KeyboardButton('/speedtest')]]
     reply_kb_markup = telegram.ReplyKeyboardMarkup(main_menu_keyborad, resize_keyboard=True, one_time_keyboard=True)
@@ -25,27 +22,34 @@ def start(bot, update):
     #Send message with menu
     bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=reply_kb_markup)
 
-# @send_typing_action
 def network(bot, update):
+    typing(bot, update)
     s = speedtest.Speedtest()
-    s.get_best_server()
-    down = s.download()
-    up = s.upload() 
-    result = s.results.dict()
 
+    typing(bot, update)
+    s.get_best_server()
+
+    typing(bot, update)
+    down = s.download()
+    
+    typing(bot, update)
+    up = s.upload() 
+    
+    typing(bot, update)
+    result = s.results.dict()
     down_mb = down / 1000000
     up_mb =  up / 1000000
 
+    typing(bot, update)
     msg = ('Ping: %d\n' %result["ping"])
     msg += ('Download: %.2f Mbps\n' % down_mb)
     msg += ('Upload: %.2f Mbps\n' % up_mb)
     update.message.reply_text(msg)
 
 #Configurando o Bot
-updater = Updater('TOKEN HERE')
+updater = Updater('TOKEN')
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('speedtest', network))
 
 updater.start_polling()
 updater.idle()
-
--- src/network.py
+++ src/network.py
@@ -1,16 +1,14 @@
 import speedtest
 
 s = speedtest.Speedtest()
-
 s.get_best_server()
 down = s.download()
-up = s.upload() 
-result = s.results.dict()
+up = s.upload()
 
+result = s.results.dict()
 down_mb = down / 1000000
 up_mb =  up / 1000000
 
 print(result["ping"])
 print('Download: %.2f Mbps' % down_mb)
 print('Upload: %.2f Mbps' % up_mb)
-
