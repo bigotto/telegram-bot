b74ae0ecbc720a136569214c10fcdbc66d92e1d1
Bruno Bigotto <bruno.bigotto@nastek.com.br>
2019-09-25 17:52:05 -0400

    Shell script para instalar as libs e configurar o autostart no RaspberryPi

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+!/bin/bash
 echo "Iniciando..."
 
-# usuario = 'id -u'
-# if [ "$usuario" != "0" ]
-# then
-#     echo -e "Usuário necessita ser root."
-#     exit
-# fi
+script_dir=`pwd`
+echo "Diretório atual: $script_dir"
 
 # Verificar se possui pip instalado
 apt-get install python-pip
 # Instalar as libs necessarias
 pip install speedtest-cli
 pip install python-telegram-bot
+
+#Configura o serviço para iniciar automaticamente
+#Backup
+cp /etc/rc.local /etc/rc.local.bak
+#Subistituindo
+cp "$script_dir"/src/rc.local /etc/rc.local
+
+echo "Concluido!"
